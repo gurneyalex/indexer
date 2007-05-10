ecf0e1dcd7864df822a9dcc5c47068b6367f27b5
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2007-05-10 11:18:00 +0200

    __future__ imports must be the first instruction

--- setup.py
+++ setup.py
@@ -17,9 +17,8 @@
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 """ Generic Setup script, takes package info from __pkginfo__.py file """
 
-__revision__ = '$Id: setup.py,v 1.4 2004-05-28 10:42:05 syt Exp $'
-
 from __future__ import nested_scopes
+
 import os
 import sys
 import shutil

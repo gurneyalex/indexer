2408c2509408685b51ee81d8b156f90aafbe168e
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2008-05-08 16:48:35 +0200

    fix test

--- test/unittest_postgres8_indexer.py
+++ test/unittest_postgres8_indexer.py
@@ -15,14 +15,14 @@ class PGIndexerTC(unittest.TestCase):
     def test_index_object(self):
         self.indexer.index_object(1, IndexableObject())
         self.assertEquals(self.cnx.received,
-                          [("INSERT INTO appears(uid, words) VALUES (%(uid)s,to_tsvector('default', %(wrds)s));",
-                            {'wrds': 'ginco jpl bla blip blop blap', 'uid': 1})])
+                          [("INSERT INTO appears(uid, words) VALUES (%(uid)s,to_tsvector(%(config)s, %(wrds)s));",
+                            {'config': 'default', 'wrds': 'ginco jpl bla blip blop blap', 'uid': 1})])
         
     def test_execute(self):
         self.indexer.execute(u'ginco-jpl')
         self.assertEquals(self.cnx.received,
-                          [("SELECT 1, uid FROM appears WHERE words @@ to_tsquery('default', %(words)s)",
-                            {'words': 'ginco&jpl'})])
+                          [("SELECT 1, uid FROM appears WHERE words @@ to_tsquery(%(config)s, %(words)s)",
+                            {'config': 'default', 'words': 'ginco&jpl'})])
 
     def test_embedded_tsearch2_is_found(self):
         # just make sure that something is found
